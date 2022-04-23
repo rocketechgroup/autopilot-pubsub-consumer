2b98c7cd0fb7549ea4b5e89d1fc4fb844eaa263c
Richard He <shenghua.he@rocketech.co.uk>
2022-04-23 22:33:11 +0100

    Add a demo message producer and a consumer

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
+import logging
 
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
 
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
 PROJECT_ID = os.environ.get('PROJECT_ID')
 SUBSCRIPTION_ID = os.environ.get('SUBSCRIPTION_ID')
-# Number of seconds the subscriber should listen for messages
-timeout = 5.0
 
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_ID)
 
-def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    print(f"Received {message.data!r}.")
+
+def callback(message) -> None:
+    logging.info(f"Received {message.data!r}.")  # IMPORTANT: do not log message content in production code
     message.ack()
+
 
 # Limit the subscriber to only have ten outstanding messages at a time.
 flow_control = pubsub_v1.types.FlowControl(max_messages=10)
@@ -28,6 +32,6 @@ with subscriber:
     try:
         # When `timeout` is not set, result() will block indefinitely,
         # unless an exception is encountered first.
-        streaming_pull_future.result(timeout=timeout)
-    except TimeoutError:`
+        streaming_pull_future.result()
+    except TimeoutError:
         streaming_pull_future.cancel()  # Trigger the shutdown.
